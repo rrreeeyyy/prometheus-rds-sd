5667b5e8290cfd388fb4f7c8e1755163b7a3156f
Ryota Yoshikawa <ryota-yoshikawa@cookpad.com>
2020-03-13 19:50:12 +0900

    Fix default interval to avoid API limit

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ const (
 var (
 	a               = kingpin.New("sd adapter usage", "Tool to generate file_sd target files for AWS RDS SD .")
 	outputFile      = a.Flag("output.file", "Output file for file_sd compatible file.").Default("rds_sd.json").String()
-	refreshInterval = a.Flag("refresh.interval", "Refresh interval to re-read the instance list.").Default("60").Int()
+	refreshInterval = a.Flag("refresh.interval", "Refresh interval to re-read the instance list.").Default("120").Int()
 	logger          log.Logger
 )
 
