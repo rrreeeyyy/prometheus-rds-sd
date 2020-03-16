deae5f5405cdf9c5b82de9b590de1f7325798f31
Ryota Yoshikawa <ryota-yoshikawa@cookpad.com>
2020-03-16 17:39:35 +0900

    Add Makefile and fix arguments

---
+++ Makefile
@@ -0,0 +1,29 @@
+VERSION = 0.1.0
+COMMIT = $(shell git describe --always)
+GOFILES_NOVENDOR = $(shell find . -type f -name '*.go' -not -path "./vendor/*")
+
+default: build
+
+# build generate binary on './bin' directory.
+build:
+	go build -ldflags "-X main.version=$(VERSION)" -o bin/prometheus-rds-sd .
+
+buildx:
+	gox -ldflags "-X main.version=$(VERSION)" -output "bin/v$(VERSION)/{{.Dir}}_{{.OS}}_{{.Arch}}" -arch "amd64" -os "linux darwin" .
+
+lint:
+	golint ${GOFILES_NOVENDOR}
+
+vet:
+	go vet -v ${GOFILES_NOVENDOR}
+
+test:
+	go test -v ${GOFILES_NOVENDOR}
+
+fmt:
+	gofmt -l -w ${GOFILES_NOVENDOR}
+
+release: buildx
+	git tag v$(VERSION)
+	git push origin v$(VERSION)
+	ghr -u rrreeeyyy v$(VERSION) bin/v$(VERSION)/
--- main.go
+++ main.go
@@ -12,12 +12,14 @@ import (
 	"github.com/prometheus/prometheus/documentation/examples/custom-sd/adapter"
 )
 
+var version string
+
 const (
 	sdName = "RDSSD"
 )
 
 var (
-	a               = kingpin.New("sd adapter usage", "Tool to generate file_sd target files for AWS RDS SD .")
+	a               = kingpin.New("RDS SD Usage", "Tool to generate file_sd target files for AWS RDS SD .")
 	outputFile      = a.Flag("output.file", "Output file for file_sd compatible file.").Default("rds_sd.json").String()
 	refreshInterval = a.Flag("refresh.interval", "Refresh interval to re-read the instance list.").Default("120").Int()
 	filters         = rdsFilters(a.Flag("filters", "The filters for describe-db-instances.").PlaceHolder("Name=NAME,Values=VALUES"))
@@ -30,6 +32,7 @@ type sdConfig struct {
 }
 
 func main() {
+	a.Version(version)
 	a.HelpFlag.Short('h')
 
 	_, err := a.Parse(os.Args[1:])
