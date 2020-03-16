54cb7d7959120488498948ef1b9127799bebc0eb
Ryota Yoshikawa <ryota-yoshikawa@cookpad.com>
2020-03-16 17:29:35 +0900

    Support filters

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/aws/aws-sdk-go/service/rds"
 	"github.com/go-kit/kit/log"
 	"gopkg.in/alecthomas/kingpin.v2"
 
@@ -19,11 +20,13 @@ var (
 	a               = kingpin.New("sd adapter usage", "Tool to generate file_sd target files for AWS RDS SD .")
 	outputFile      = a.Flag("output.file", "Output file for file_sd compatible file.").Default("rds_sd.json").String()
 	refreshInterval = a.Flag("refresh.interval", "Refresh interval to re-read the instance list.").Default("120").Int()
+	filters         = rdsFilters(a.Flag("filters", "The filters for describe-db-instances.").PlaceHolder("Name=NAME,Values=VALUES"))
 	logger          log.Logger
 )
 
 type sdConfig struct {
 	RefreshInterval int
+	Filters         []*rds.Filter
 }
 
 func main() {
@@ -42,6 +45,7 @@ func main() {
 
 	cfg := sdConfig{
 		RefreshInterval: *refreshInterval,
+		Filters:         *filters,
 	}
 
 	disc, err := newDiscovery(cfg, logger)
